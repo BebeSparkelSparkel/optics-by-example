646612fd2cfbf4bfd6dd7c314c9e99a0fa5c50b5
William Rusnack <rusnackw@umich.edu>
2020-02-21 16:44:12 -0500

    completed ch 9 sec 2

--- test/Exercises/Ch9Prisms/Sec2WritingCustomPrismsSpec.hs
+++ test/Exercises/Ch9Prisms/Sec2WritingCustomPrismsSpec.hs
@@ -1,58 +1,67 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module Exercises.Ch9Prisms.Sec2WritingCustomPrismsSpec where
 
 import Test.Hspec
 import Control.Lens
-import Test.QuickCheck
+import Data.List
 
+default (Int)
 
-_Factor :: Int -> Prism' Int Int
-_Factor x = prism' embed match
-  where
-    embed = (* x)
-    match y = if y `mod` x == 0
-      then Just y
-      else Nothing
-
-fizz :: Prism' String Int
-fizz = prism' embed match
-  where
-    embed x = if x `mod` 3 == 0
-      then "fizz"
-      else ""
-    match "fizz" = Just 3
-    match _ = Nothing
-
-buzz :: Prism' String Int
-buzz = prism' embed match
-  where
-    embed x = if x `mod` 5 == 0
-      then "buzz"
-      else ""
-    match "buzz" = Just 5
-    match _ = Nothing
 
 spec :: Spec
-spec = focus do
-  describe "_Factor" do
-    it "review" $ property \f i -> (_Factor f # i) `shouldBe` (f * i)
-    it "is match" $ property \(Positive f) i -> has (_Factor f) (f * i)
+spec = do
+  -- 1. Try to write a custom prism for matching on the tail of a list: _Tail :: Prism' [a] [a].  Is this possible? Why or why not?
+  -- It isn't possible since tail cannot reconstruct the origional list.
 
-  describe "fizzbuzz" do
-    describe "fizz" do
-      it "review" do
-        (fizz # 3) `shouldBe` "fizz"
-        (fizz # 4) `shouldBe` ""
+  it "2. Implement _Cons for lists using prism: _ListCons" do
+    let _ListCons :: Prism [a] [b] (a, [a]) (b, [b])
+        _ListCons = prism embed match
+          where
+            embed :: (b, [b]) -> [b]
+            embed = uncurry (:)
+            match :: [a] -> Either [b] (a, [a])
+            match (x:xs) = Right (x, xs)
+            match [] = Left []
+    ([] ^? _ListCons) `shouldBe` Nothing
+    ([1,2] ^? _ListCons) `shouldBe` Just (1,[2])
+    (_ListCons # (1,[])) `shouldBe` [1]
+    (_ListCons # (1,[2,3])) `shouldBe` [1,2,3]
 
-      it "match" do
-        ("fizz" ^? fizz) `shouldBe` Just 3
-        ("buzz" ^? fizz) `shouldBe` Nothing
+  describe "BONUS 3. Implement _Cycles which detects whether a list consists of exactly ‘n’ repetitions of a pattern. It should behave as follows:" do
+    let _Cycles :: Int -> Prism String String String String
+        _Cycles n = prism (embed n) match
+          where
+            embed :: Int -> String -> String
+            embed 0 _ = mempty
+            embed x s = s <> embed (x - 1) s
+            match :: String -> Either String String
+            match s | m 0 s = Right prefix
+                    | otherwise = Left s
+              where 
+                prefix = take (l `div` n) s
+                l = length s
+                m c [] | c == n = True
+                m c xs = case stripPrefix prefix xs of
+                  Just ys -> m (c + 1) ys
+                  Nothing -> False
 
-    describe "buzz" do
-      it "review" do
-        (buzz # 5) `shouldBe` "buzz"
-        (buzz # 4) `shouldBe` ""
+    it "Find the subsequence which has been repeated exactly times." $
+      ("dogdogdog" ^? _Cycles 3) `shouldBe` Just "dog"
 
-      it "match" do
-        ("buzz" ^? buzz) `shouldBe` Just 5
-        ("fizz" ^? buzz) `shouldBe` Nothing
+    it "The input isn't exactly 3 cycles of some input (it's 4), so we don't match" $
+      ("dogdogdogdog" ^? _Cycles 3) `shouldBe` Nothing
+
+    it "The input is exactly 3 cycles of \"a\"" $
+      ("aaa" ^? _Cycles 3) `shouldBe` Just "a"
+
+    it "The input isn't a cycle at all." $
+      ("xyz" ^? _Cycles 3) `shouldBe` Nothing
+
+    it "We can review to create cycles." $
+      (_Cycles 3 # "dog") `shouldBe` "dogdogdog"
+
+    it "We can mutate the sequence that's cycled" $
+      ("dogdogdog" & _Cycles 3 .~ "cats") `shouldBe` "catscatscats"
+
+-- • Can you implement a version of _Cycles which doesn’t depend on a specific iteration count? Why or why not?
 
